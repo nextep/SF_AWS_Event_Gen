3bdda469a311c60062c6272934aed4fc0d6a04ef
Elias Ibrahim <elie.ibrahim@gmail.com>
2023-05-19 15:41:34 -0400

    Update S3_SF_INT_new.sql

--- S3_SF_INT_new.sql
+++ S3_SF_INT_new.sql
@@ -12,7 +12,15 @@ CREATE STORAGE INTEGRATION s3_int
   URL = 's3://events169/To_Process'
   STORAGE_INTEGRATION = s3_int;
 
-  
+  create TABLE STAGING (
+	TIMESTAMP TIMESTAMP_NTZ(9),
+	SOURCE_IP VARCHAR(26),
+	DESTINATION_IP VARCHAR(26),
+  source_hostname VARCHAR(255),
+  destination_hostname VARCHAR(255),
+  data_source VARCHAR(255)
+);
+
 COPY INTO STAGING
 FROM (
     select $1:timestamp::timestamp as Timestamp, $1:source_ip::string as Source_IP, $1:destination_ip::string as Destination_IP, $1:source_hostname::string as Source_hostname, $1:destination_hostname::string as Destination_hostname, $1:data_source::string as Data_Source from @EVT_2_SF (file_format => JSON)
