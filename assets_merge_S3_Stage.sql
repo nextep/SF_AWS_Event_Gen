550dae6df6bd59bbfd71aa63dfe3494c1a361c60
Elias Ibrahim <elie.ibrahim@gmail.com>
2023-05-19 15:41:11 -0400

    Update assets_merge_S3_Stage.sql

--- assets_merge_S3_Stage.sql
+++ assets_merge_S3_Stage.sql
@@ -17,9 +17,9 @@ MERGE INTO ASSETS AS target
 USING (
     SELECT source_ip, destination_ip, destination_hostname, source_hostname,  (SELECT MIN(timestamp) FROM staging s WHERE s.source_ip = t.source_ip AND s.destination_ip = t.destination_ip) AS first_seen ,(SELECT MAX(timestamp) FROM staging s WHERE s.source_ip = t.source_ip AND s.destination_ip = t.destination_ip) AS last_seen, data_source
     FROM staging t
-    GROUP BY source_ip, destination_ip, destination_hostname, source_hostname,data_source
+    GROUP BY source_ip, destination_ip, destination_hostname, source_hostname, data_source
     ) AS source
-ON (target.source_ip = source.source_ip AND target.destination_ip = source.destination_ip AND target.source_hostname = source.source_hostname AND target.destination_hostname = source.destination_hostname )
+ON (target.source_ip = source.source_ip AND target.destination_ip = source.destination_ip AND target.source_hostname = source.source_hostname AND target.destination_hostname = source.destination_hostname AND target.data_source = source.data_source)
 WHEN MATCHED THEN
   UPDATE SET target.added_on = current_timestamp,
              target.first_seen = GREATEST(target.first_seen, source.first_seen),
